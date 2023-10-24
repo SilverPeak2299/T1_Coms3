402e2e2dfcc54855bbe5b4b92952433bc7c2e423
SilverPeak2299 <dannydickerson03@gmail.com>
2023-10-24 11:48:14 +1100

    LCD codeready for testing

---
+++ src/Main/Lcd.ino
@@ -0,0 +1,22 @@
+// Controlls IC2 LED
+
+//variables
+
+extern int marble_count;
+
+void init_lcd() {
+  lcd.begin(16,2);
+  lcd.init();
+  lcd.backlight();
+  Serial.begin(9600);
+
+  lcd.setCursor(0,0);
+  lcd.print("marbleCt: ");
+  lcd.print( String(marble_count) );
+}
+
+void update_lcd() {
+  lcd.setCursor(0,0);
+  lcd.print("marbleCt: ");
+  lcd.print( String(marble_count) );
+}
--- src/Main/Main.ino
+++ src/Main/Main.ino
@@ -1,9 +1,49 @@
-void setup() {
-  // put your setup code here, to run once:
 
+//Main File for the arduino project
+#include <LiquidCrystal_I2C.h>
+
+// Defining Pins
+#define entry_input 2
+#define exit_input 3
+//function prototypes
+
+
+// Components
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+// Variables
+int marble_count {0};
+
+
+void setup() {
+  //pin settup
+  pinMode(entry_input, INPUT_PULLUP);
+  pinMode(exit_inpit, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(entry_input), marble_entry, RISING);
+  attachInterrupt(digitalPinToInterrupt(exit_input), marble_exit, RISING);
+
+  // settup funcitons
+  init_lcd(); // Setting up the LCD in lcd.ino
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+// Interupt Functions
+
+void marble_entry() {
+  marble_count++;
+ 
+  update_lcd();
+}
+
+void marble_exit () {
+  marble_count--;
+
+  update_lcd();
+}
+
+
